d29d62e9de77df50e2572990b5634df936aca105
Fedor Logachev <not.fl3@gmail.com>
2020-11-22 13:59:33 -0600

    quad-gl: Color::to_vec/from_vec helper functions

--- examples/life.rs
+++ examples/life.rs
@@ -1,77 +1,334 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("Life")]
+#[macroquad::main("Fire")]
 async fn main() {
-    let w = screen_width() as usize;
-    let h = screen_height() as usize;
+    let w = 50usize;
+    let h = 30usize;
 
-    let mut buffer = vec![WHITE; w * h];
-    let mut image = Image::gen_image_color(w as u16, h as u16, WHITE);
-    let image_data = image.get_image_data_mut();
-    for y in 0..h {
-        for x in 0..w {
-            if rand::gen_range(0, 5) == 0 {
-                image_data[y * w + x] = BLACK;
-            }
-        }
-    }
+    let mut image = Image::gen_image_color(w as u16, (h - 2) as u16, BLACK);
+
     let texture = load_texture_from_image(&image);
+    set_texture_filter(texture, FilterMode::Nearest);
 
+    let mut fire = vec![0u8; w * h];
+    let mut prev_fire = vec![0u8; w * h];
+
+    let mut acc = 0.0;
     loop {
-        clear_background(WHITE);
+        acc += get_frame_time();
 
-        let w = image.width();
-        let h = image.height();
+        clear_background(BLACK);
 
-        let image_data = image.get_image_data();
-        for y in 0..h as i32 {
-            for x in 0..w as i32 {
-                let mut neighbors_count = 0;
+        set_camera(Camera2D::from_display_rect(Rect::new(
+            0.0,
+            0.0,
+            w as _,
+            (h - 5) as _,
+        )));
 
-                for j in -1i32..=1 {
-                    for i in -1i32..=1 {
-                        // out of bounds
-                        if y + j < 0 || y + j >= h as i32 || x + i < 0 || x + i >= w as i32 {
-                            continue;
-                        }
-                        // cell itself
-                        if i == 0 && j == 0 {
-                            continue;
-                        }
+        let mut image_data = image.get_image_data_mut();
 
-                        let neighbor = image_data[(y + j) as usize * w + (x + i) as usize];
-                        if neighbor == BLACK {
-                            neighbors_count += 1;
-                        }
+        if acc > 0.1 {
+            for i in w + 1..(h - 1) * w - 1 {
+                let sum = prev_fire[i - w - 1] as i32
+                    + prev_fire[i - w] as i32
+                    + prev_fire[i - w + 1] as i32
+                    + prev_fire[i - 1] as i32
+                    + prev_fire[i + 1] as i32
+                    + prev_fire[i + w - 1] as i32
+                    + prev_fire[i + w] as i32
+                    + prev_fire[i + w + 1] as i32;
+                let mut avg = (sum / 8) as u8;
+ 
+                if rand::gen_range(0, 5) == 0 && (avg > 0 || i >= (h - 4) * w) {
+                    if avg >= 1 {
+                        avg -= 1;
+                    } else {
+                        avg = 255;
                     }
                 }
-
-                let current_cell = image_data[y as usize * w + x as usize];
-                buffer[y as usize * w + x as usize] = match (current_cell, neighbors_count) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (BLACK, x) if x < 2 => WHITE,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (BLACK, 2) | (BLACK, 3) => BLACK,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (BLACK, x) if x > 3 => WHITE,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (WHITE, 3) => BLACK,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
-                };
+                fire[i] = avg;
             }
+
+            for x in 0..(h - 1) * w {
+                prev_fire[x as usize] = fire[x + w];
+            }
+
+            for i in (h - 7) * w..(h - 1) * w {
+                if fire[i] < 15 {
+                    fire[i] = 22 - fire[i];
+                }
+            }
+
+            for i in 0..(h - 2) * w {
+                image_data[i] = PALETTE[fire[i] as usize];
+            }
+
+            update_texture(texture, &image);
+
+            acc = 0.0;
         }
-
-        image.update(&buffer);
-
-        update_texture(texture, &image);
-
         draw_texture(texture, 0., 0., WHITE);
 
         next_frame().await
     }
 }
+
+const PALETTE: [Color; 256] = [
+    Color::new_const(0, 0, 0, 255),
+    Color::new_const(0, 1, 1, 255),
+    Color::new_const(0, 4, 5, 255),
+    Color::new_const(0, 7, 9, 255),
+    Color::new_const(0, 8, 11, 255),
+    Color::new_const(0, 9, 12, 255),
+    Color::new_const(15, 6, 8, 255),
+    Color::new_const(25, 4, 4, 255),
+    Color::new_const(33, 3, 3, 255),
+    Color::new_const(40, 2, 2, 255),
+    Color::new_const(48, 2, 2, 255),
+    Color::new_const(55, 1, 1, 255),
+    Color::new_const(63, 0, 0, 255),
+    Color::new_const(63, 0, 0, 255),
+    Color::new_const(63, 3, 0, 255),
+    Color::new_const(63, 7, 0, 255),
+    Color::new_const(63, 10, 0, 255),
+    Color::new_const(63, 13, 0, 255),
+    Color::new_const(63, 16, 0, 255),
+    Color::new_const(63, 20, 0, 255),
+    Color::new_const(63, 23, 0, 255),
+    Color::new_const(63, 26, 0, 255),
+    Color::new_const(63, 29, 0, 255),
+    Color::new_const(63, 33, 0, 255),
+    Color::new_const(63, 36, 0, 255),
+    Color::new_const(63, 39, 0, 255),
+    Color::new_const(63, 39, 0, 255),
+    Color::new_const(63, 40, 0, 255),
+    Color::new_const(63, 40, 0, 255),
+    Color::new_const(63, 41, 0, 255),
+    Color::new_const(63, 42, 0, 255),
+    Color::new_const(63, 42, 0, 255),
+    Color::new_const(63, 43, 0, 255),
+    Color::new_const(63, 44, 0, 255),
+    Color::new_const(63, 44, 0, 255),
+    Color::new_const(63, 45, 0, 255),
+    Color::new_const(63, 45, 0, 255),
+    Color::new_const(63, 46, 0, 255),
+    Color::new_const(63, 47, 0, 255),
+    Color::new_const(63, 47, 0, 255),
+    Color::new_const(63, 48, 0, 255),
+    Color::new_const(63, 49, 0, 255),
+    Color::new_const(63, 49, 0, 255),
+    Color::new_const(63, 50, 0, 255),
+    Color::new_const(63, 51, 0, 255),
+    Color::new_const(63, 51, 0, 255),
+    Color::new_const(63, 52, 0, 255),
+    Color::new_const(63, 53, 0, 255),
+    Color::new_const(63, 53, 0, 255),
+    Color::new_const(63, 54, 0, 255),
+    Color::new_const(63, 55, 0, 255),
+    Color::new_const(63, 55, 0, 255),
+    Color::new_const(63, 56, 0, 255),
+    Color::new_const(63, 57, 0, 255),
+    Color::new_const(63, 57, 0, 255),
+    Color::new_const(63, 58, 0, 255),
+    Color::new_const(63, 58, 0, 255),
+    Color::new_const(63, 59, 0, 255),
+    Color::new_const(63, 60, 0, 255),
+    Color::new_const(63, 60, 0, 255),
+    Color::new_const(63, 61, 0, 255),
+    Color::new_const(63, 62, 0, 255),
+    Color::new_const(63, 62, 0, 255),
+    Color::new_const(63, 63, 0, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+    Color::new_const(63, 63, 63, 255),
+];
